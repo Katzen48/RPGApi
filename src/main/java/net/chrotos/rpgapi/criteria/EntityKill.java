d3aea1867fce99fd295383b63b3c8067cac20341
Katzen48 | Tobi <Katzen48@freenet.de>
2022-02-19 00:19:08 +0100

    fixed superbuilder, bumped shadow plugin version

--- src/main/java/net/chrotos/rpgapi/criteria/EntityKill.java
+++ src/main/java/net/chrotos/rpgapi/criteria/EntityKill.java
@@ -1,11 +1,11 @@
 package net.chrotos.rpgapi.criteria;
 
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NonNull;
+import lombok.experimental.SuperBuilder;
 
 @Getter
-@Builder
+@SuperBuilder
 public class EntityKill extends EntityCriterion {
     /**
      * The count, of entities to be killed (requires type to be set, should not be used with id)
