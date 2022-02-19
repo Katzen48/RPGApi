0d831ef57577c64ac5a96872ce4764b8b2abc84a
Katzen48 | Tobi <Katzen48@freenet.de>
2022-02-19 12:26:08 +0100

    fixed superbuilder

--- src/main/java/net/chrotos/rpgapi/criteria/Criterion.java
+++ src/main/java/net/chrotos/rpgapi/criteria/Criterion.java
@@ -1,9 +1,11 @@
 package net.chrotos.rpgapi.criteria;
 
 import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 import net.chrotos.rpgapi.quests.QuestCriterion;
 
 @Setter
+@SuperBuilder
 public class Criterion {
     /**
      * The quest criterion, this criterion is part of.
